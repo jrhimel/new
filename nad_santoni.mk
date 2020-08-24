121d73ebc88db97816770f67578f8d1da4e4302f
Jubayer Himel <64546040+jrhimel@users.noreply.github.com>
2020-08-24 19:53:51 +0600

    Bring up Corvus

--- nad_santoni.mk
+++ nad_santoni.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2019 The NUSANTARA-OS Project
+# Copyright (C) 2019 The Corvus-OS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,22 +16,23 @@
 
 $(call inherit-product, device/xiaomi/santoni/full_santoni.mk)
 
-# Inherit some common NUSANTARA-OS stuff.
-$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
+# Inherit some common Corvus-OS stuff.
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
 
 PRODUCT_DEVICE := santoni
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 4
-PRODUCT_NAME := nad_santoni
+PRODUCT_NAME := du_santoni
 BOARD_VENDOR := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
 
 # OFFICIAL TAG
-NAD_BUILD_TYPE := OFFICIAL
+DU_BUILD_TYPE := OFFICIAL
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
+TARGET_BOOT_ANIMATION_RES := 720
 	
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
